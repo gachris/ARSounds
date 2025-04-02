4b8b3d451acd84532da8f8d78136839f1c72ed7b
Christos Gkatzos <christos.gkatzos@hotmail.com>
2025-04-02 19:32:05 +0300

    in progress

--- src/ARSounds.Web.App/arsounds.web.app.client/aspnetcore-https.js
+++ src/ARSounds.Web.App/arsounds.web.app.client/aspnetcore-https.js
@@ -19,6 +19,10 @@ if (!certificateName) {
 const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
+if (!fs.existsSync(baseFolder)) {
+  fs.mkdirSync(baseFolder, { recursive: true });
+}
+
 if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
   spawn('dotnet', [
     'dev-certs',
@@ -29,5 +33,5 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
     'Pem',
     '--no-password',
   ], { stdio: 'inherit', })
-  .on('exit', (code) => process.exit(code));
+    .on('exit', (code) => process.exit(code));
 }
